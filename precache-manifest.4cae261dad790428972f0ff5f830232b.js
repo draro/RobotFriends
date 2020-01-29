315d7c90a8fc8dbcf219fa9f6600ef84c9e5f164
draro <58738792+draro@users.noreply.github.com>
2020-01-29 14:47:01 -0500

    Updates

---
+++ precache-manifest.4cae261dad790428972f0ff5f830232b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c02fe65d2137f9b63f5cad44099e671b",
+    "url": "/RobotFriends/index.html"
+  },
+  {
+    "revision": "343e548883abee5138fa",
+    "url": "/RobotFriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "b07d75c0db6cd7b8eed9",
+    "url": "/RobotFriends/static/css/main.9289730c.chunk.css"
+  },
+  {
+    "revision": "343e548883abee5138fa",
+    "url": "/RobotFriends/static/js/2.9b0ba1ee.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/RobotFriends/static/js/2.9b0ba1ee.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b07d75c0db6cd7b8eed9",
+    "url": "/RobotFriends/static/js/main.265f4498.chunk.js"
+  },
+  {
+    "revision": "54efe41faa812805187b",
+    "url": "/RobotFriends/static/js/runtime-main.f6d23a59.js"
+  }
+]);
